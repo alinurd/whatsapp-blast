06dd4a30206a51b7e31fe5dbc2773dc89ab507b1
ali dev14 <ali.nrdn14005@gmail.com>
2024-03-10 16:44:47 +0700

    ok

--- resources/views/partials/dashboard/_body_header.blade.php
+++ resources/views/partials/dashboard/_body_header.blade.php
@@ -38,7 +38,7 @@
       @if(Auth::user()->role ==2 || Auth::user()->role ==1 )
 
       <li class="nav-item me-3">
-        <a class="btn btn-primary d-flex align-items-center" aria-current="page" href="https://hopeui.iqonic.design/pro/laravel" target="_blank" style="top: 4px;">
+        <a class="btn btn-primary d-flex align-items-center" aria-current="page" href="{{route('muzakki.index')}}" target="" style="top: 4px;">
         <svg width="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">                                <path d="M21.6389 14.3957H17.5906C16.1042 14.3948 14.8993 13.1909 14.8984 11.7045C14.8984 10.218 16.1042 9.01409 17.5906 9.01318H21.6389" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>                                <path d="M18.049 11.6429H17.7373" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>                                <path fill-rule="evenodd" clip-rule="evenodd" d="M7.74766 3H16.3911C19.2892 3 21.6388 5.34951 21.6388 8.24766V15.4247C21.6388 18.3229 19.2892 20.6724 16.3911 20.6724H7.74766C4.84951 20.6724 2.5 18.3229 2.5 15.4247V8.24766C2.5 5.34951 4.84951 3 7.74766 3Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>                                <path d="M7.03516 7.5382H12.4341" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>                            </svg>  &ensp;                       
             Muzakki
         </a>
@@ -47,7 +47,7 @@
 
         @if(Auth::user()->role ==3 || Auth::user()->role ==1 )
       <li class="nav-item me-3">
-        <a class="btn btn-secondary d-flex align-items-center" aria-current="page" href="https://hopeui.iqonic.design/pro/laravel" target="_blank" style="top: 4px;">
+        <a class="btn btn-secondary d-flex align-items-center" aria-current="page" href="https://hopeui.iqonic.design/pro/laravel" href="{{route('mustahik.index')}}" target="" style="top: 4px;">
         <svg width="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">                            <path fill-rule="evenodd" clip-rule="evenodd" d="M17.7689 8.3818H22C22 4.98459 19.9644 3 16.5156 3H7.48444C4.03556 3 2 4.98459 2 8.33847V15.6615C2 19.0154 4.03556 21 7.48444 21H16.5156C19.9644 21 22 19.0154 22 15.6615V15.3495H17.7689C15.8052 15.3495 14.2133 13.7975 14.2133 11.883C14.2133 9.96849 15.8052 8.41647 17.7689 8.41647V8.3818ZM17.7689 9.87241H21.2533C21.6657 9.87241 22 10.1983 22 10.6004V13.131C21.9952 13.5311 21.6637 13.8543 21.2533 13.8589H17.8489C16.8548 13.872 15.9855 13.2084 15.76 12.2643C15.6471 11.6783 15.8056 11.0736 16.1931 10.6122C16.5805 10.1509 17.1573 9.88007 17.7689 9.87241ZM17.92 12.533H18.2489C18.6711 12.533 19.0133 12.1993 19.0133 11.7877C19.0133 11.3761 18.6711 11.0424 18.2489 11.0424H17.92C17.7181 11.0401 17.5236 11.1166 17.38 11.255C17.2364 11.3934 17.1555 11.5821 17.1556 11.779C17.1555 12.1921 17.4964 12.5282 17.92 12.533ZM6.73778 8.3818H12.3822C12.8044 8.3818 13.1467 8.04812 13.1467 7.63649C13.1467 7.22487 12.8044 6.89119 12.3822 6.89119H6.73778C6.31903 6.89116 5.9782 7.2196 5.97333 7.62783C5.97331 8.04087 6.31415 8.37705 6.73778 8.3818Z" fill="currentColor"></path>                            </svg>  &ensp;                       
             Mustahiq
         </a>
