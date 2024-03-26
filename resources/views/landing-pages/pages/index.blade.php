c9dfc2f29d1e2d37876cbb4511d7ef2c2f537fec
ali dev14 <ali.nrdn14005@gmail.com>
2024-03-26 19:17:35 +0700

    ok

--- resources/views/landing-pages/pages/index.blade.php
+++ resources/views/landing-pages/pages/index.blade.php
@@ -13,6 +13,20 @@
                     </div>
                 </div>
             </div>
+
+            @if (session('success'))
+            <div class="toast fade show" role="alert" aria-live="assertive" aria-atomic="true">
+                <div class="toast-header">
+                    <svg class="bd-placeholder-img rounded me-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#007aff"></rect></svg>
+                    <strong class="me-auto">Pengajuan Mustahiq</strong>
+                    <small class="text-muted text-success">Successfully</small>
+                    <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                </div>
+                <div class="toast-body">
+                Pengajuan anda berhasil ditambahkan
+                </div>
+            </div>
+         @endif
             <div class="iq-header-img">
                 <img src="{{asset('images/dashboard/top-header.png')}}" alt="header" class="theme-color-default-img img-fluid w-100 h-100 animated-scaleX">
                 <img src="{{asset('images/dashboard/top-header1.png')}}" alt="header" class="theme-color-purple-img img-fluid w-100 h-100 animated-scaleX">
