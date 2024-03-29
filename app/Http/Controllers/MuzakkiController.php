afc3bf4386cdd61845c77de2151592dfe2dcd2e6
ali dev14 <ali.nrdn14005@gmail.com>
2024-03-29 22:43:55 +0700

    ok

--- app/Http/Controllers/MuzakkiController.php
+++ app/Http/Controllers/MuzakkiController.php
@@ -103,7 +103,7 @@ $payload = json_encode([
 $n=[$dUser->nama_lengkap,$MuzakkiHeader->code];
 $msg = "Alhamdulillah, telah diterima penunaikan zis/fidyah dari Bapak/ibu: " . $dibayarkan->nama_lengkap . ".\n";
 $msg .= "No. Invoice: #" . $MuzakkiHeader->code . ".\n.\n.\n ";
-$msg .= "Lihat detail: https://zis-alhasanah.com/showinvoice/" . $MuzakkiHeader->code . "";
+$msg .= "Lihat detail: https://zis-alhasanah.com/showinvoice/" . $MuzakkiHeader->code;
 $this->cetakinvoice($MuzakkiHeader->code);
 
  $this->sendMassage1($no,$msg, $MuzakkiHeader->code);
