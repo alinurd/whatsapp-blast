a42b08a28adba9a7478f806d387ffeecb7e37733
ali dev14 <ali.nrdn14005@gmail.com>
2024-03-29 16:49:36 +0700

    ok

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -47,7 +47,7 @@ class Controller extends BaseController
             'destination' => '6285817069096',
             'media_url' => 'https://zis-alhasanah.com/public/invoice/invoice_'.$code.'.pdf',
             // 'media_url' => 'http://127.0.0.1:8000/public/images/icons/invoice-6.pdf',
-            'message' => 'Alhamdulillah, telah diterima penunaikan zis/fidyah dari Bapak/ibu:'.$from,
+            // 'message' => 'Alhamdulillah, telah diterima penunaikan zis/fidyah dari Bapak/ibu:'.$from,
             'include_unsubscribe' => false,
         ]);
 
