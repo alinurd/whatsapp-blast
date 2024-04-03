27d2f79c5927ff369a9ac18ac4d13ff37b011fbf
ali dev14 <ali.nrdn14005@gmail.com>
2024-04-03 15:33:07 +0700

    ok

--- app/Http/Controllers/MuzakkiController.php
+++ app/Http/Controllers/MuzakkiController.php
@@ -106,7 +106,7 @@ $n=[$dUser->nama_lengkap,$MuzakkiHeader->code];
 $msg = "Alhamdulillah, telah diterima penunaikan zis/fidyah dari Bapak/ibu: " . $dibayarkan->nama_lengkap . ".\n";
 $msg .= "No. Invoice: #" . $MuzakkiHeader->code . "\n\n\n ";
 $msg .= "Lihat detail: https://zis-alhasanah.com/showinvoice/" . $MuzakkiHeader->code;
-// $this->cetakinvoice($MuzakkiHeader->code);
+$this->cetakinvoice($MuzakkiHeader->code);
 
  $this->sendMassage1($no,$msg, $MuzakkiHeader->code);
 //  $this->sendMassage($no,$msg);
