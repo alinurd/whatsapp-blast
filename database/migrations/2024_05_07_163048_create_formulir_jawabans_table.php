fdb5f094053e93df19158b12bf6d6f0339b93265
dwikybp2 <dwikybagusprasetyo@gmail.com>
2024-05-12 23:21:18 +0700

    fix: form & report

--- database/migrations/2024_05_07_163048_create_formulir_jawabans_table.php
+++ database/migrations/2024_05_07_163048_create_formulir_jawabans_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('formulir_jawabans', function (Blueprint $table) {
             $table->id();
             $table->integer('formulir_id');
+            $table->integer('product_id');
+            $table->integer('kategori_id');
             $table->integer('parent_id')->nullable();
             $table->string('variabel');
             $table->text('jawaban')->nullable();
--- resources/views/formulir/excel.blade.php
+++ resources/views/formulir/excel.blade.php
@@ -1,6 +1,12 @@
+@if ($jawaban->isEmpty())
+    <script>
+        alert('No data found.');
+        window.close();
+    </script>
+@endif
 
 <table class="table table-striped">
-    <tr style="text-align:center;">
+    <!-- <tr style="text-align:center;">
         <td>No</td>
         <td>Kategori</td>
         <td>Soal</td>
@@ -13,12 +19,39 @@
             <td>{{ $jwb->variabel }}</td>
             <td>
                 @if ($jwb->formulir->formulir_tipe == 'file')
-                    <a href="{{ asset('upload/'.$jwb->jawaban) }}" target="_blank" rel="noopener noreferrer">{{ $jwb->jawaban }}</a>
+                    <a href="{{ asset('storage/upload/'.$jwb->jawaban) }}" target="_blank" rel="noopener noreferrer">{{ $jwb->jawaban }}</a>
                 @else
                     {{ $jwb->jawaban != 1 ? $jwb->jawaban : $formulirOption[$jwb->checkbox_id] }}
                 @endif
             </td>
         </tr>
-    @endforeach
+    @endforeach -->
 
+    <tr style="text-align:left;">
+        <td>Kategori</td>
+        <td></td>
+        <td></td>
+        <td>{{ $formulir[0]->formulir_nama }}</td>
+    </tr>
+    <tr style="text-align:left;">
+        <td>Nama Form</td>
+        <td></td>
+        <td></td>
+        <td>{{ $formulir[0]->parent->nama }}</td>
+    </tr>
+    <br>
+
+    <tr style="text-align:left;">
+        @foreach ($formulir as $form)
+            <td>{{ $form->formulir_nama }}</td>
+        @endforeach
+    </tr>
+
+    @foreach ($jwbFilter as $jb)
+        <tr>
+            @foreach($jb as $val)
+                <td>{{ $val }}</td>
+            @endforeach
+        </tr>
+    @endforeach
 </table>
