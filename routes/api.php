1ccb23d96b63390e49c7c699e9d4ad01ffb32057
ali dev14 <ali.nrdn14005@gmail.com>
2025-05-06 10:51:33 +0700

    update api

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('webhook', RespondapiController::class); 
+// Route::apiResource('webhook', RespondapiController::class); 
+Route::post('/webhook', [RespondapiController::class, 'store']);
